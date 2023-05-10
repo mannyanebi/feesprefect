d5f58558857d9dd962f87602b08b87f194264e35
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-05-10 13:24:47 +0100

    add: migration of data to elephantsql

--- feesprefect/settings/local.py
+++ feesprefect/settings/local.py
@@ -1,6 +1,9 @@
 # Monkey patching with Django Stubs
 # import django_stubs_ext
 
+import dj_database_url
+from decouple import config
+
 from .base import *  # noqa, pylint: disable=wildcard-import, unused-wildcard-import
 
 # django_stubs_ext.monkeypatch()
@@ -19,9 +22,20 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "feesprefect.local.sqlite3",
+#     }
+# }
+
+DB_NAME = config("DB_NAME")
+DB_USER = config("DB_USER")
+DB_PASSWORD = config("DB_PASSWORD")
+DB_HOST = config("DB_HOST")
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "feesprefect.local.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
 }
