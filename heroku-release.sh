fd1843b530241d82dcec4cb7f93e8fe2e8d12a43
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-05-20 21:28:04 +0100

    fix: for collectstatic error

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,3 +1,7 @@
+echo "Running heroku-release.sh"
 cd src
+echo "Printing pwd"
+pwd
+python manage.py collectstatic --noinput
 python manage.py check --deploy --fail-level WARNING
 python manage.py migrate
