de217391dc45234c1ee720fe99997a4030b17796
Emmanuel Anebi <anebiemmanuel@gmail.com>
2023-05-20 21:58:03 +0100

    fix: still fixing deployment

--- heroku-release.sh
+++ heroku-release.sh
@@ -1,5 +1,9 @@
-cd app
 ls -la
+pwd
+cd static
+pwd
+cd ../
+mkdir feesprefect/static
 python manage.py collectstatic --noinput
 python manage.py check --deploy --fail-level WARNING
 python manage.py migrate
